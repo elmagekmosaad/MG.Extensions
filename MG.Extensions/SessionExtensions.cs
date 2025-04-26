b599004c5e99f3d24c0068f40d84b2eb33bc82f7
elmagek mosaad <elmagekmosaad3535@gmail.com>
2025-04-26 13:55:21 +0300

    Add project files.

---
+++ MG.Extensions/SessionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace MG.Extensions.Session
+{
+    public static class SessionExtensions
+    {
+        public static void SetUserSessionData(this ISession session, UserSessionData userSessionData)
+        {
+            session.SetString("UserSessionData", JsonConvert.SerializeObject(userSessionData));
+        }
+
+        public static UserSessionData GetUserSessionData(this ISession session)
+        {
+            var userSessionDataJson = session.GetString("UserSessionData");
+            return userSessionDataJson == null ? null : JsonConvert.DeserializeObject<UserSessionData>(userSessionDataJson);
+        }
+    }
+}
