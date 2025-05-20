bc22f2f43f7ef33a932d14e8a2a887242671db7a
elmagek mosaad <elmagekmosaad3535@gmail.com>
2025-05-20 07:16:30 +0300

    refactor: add user.FindFirstValue("Uid") to getUserId

--- MG.Extensions/ClaimsPrincipalExtensions.cs
+++ MG.Extensions/ClaimsPrincipalExtensions.cs
@@ -1,17 +1,28 @@
 ﻿using System.Security.Claims;
 
-namespace MG.Extensions.Session
+namespace MG.Extensions.Claims
 {
     public static class ClaimsPrincipalExtensions
     {
         public static string GetUserId(this ClaimsPrincipal user)
-            => user.FindFirstValue(ClaimTypes.NameIdentifier);
+            => user.FindFirstValue("Uid") ?? 
+            user.FindFirstValue(ClaimTypes.NameIdentifier);
 
         public static string GetUserName(this ClaimsPrincipal user)
             => user.Identity?.Name;
 
         public static string GetFullName(this ClaimsPrincipal user)
-            => user.FindFirstValue("FullName");
+            => user.FindFirstValue(ClaimTypes.Name) ??
+               user.FindFirstValue("FullName") ??
+               user.FindFirstValue("name") ?? string.Empty;
+
+        public static string GetEmail(this ClaimsPrincipal user)
+            => user.FindFirstValue(ClaimTypes.Email) ??
+               user.FindFirstValue("email") ?? string.Empty;
+
+        public static string GetRole(this ClaimsPrincipal user)
+            => user.FindFirstValue(ClaimTypes.Role) ??
+               user.FindFirstValue("role") ?? string.Empty;
     }
 
 }
