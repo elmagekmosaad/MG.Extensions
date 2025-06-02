bd64d143de8db496f19058728e28cceb51149bfc
elmagek mosaad <elmagekmosaad3535@gmail.com>
2025-06-02 23:12:52 +0300

    Add default values for Avatar and Roles in session data
    Enhance the `SetUserSessionData` method in the `SessionExtensions` class to ensure that the `UserSessionData` object has default values for the `Avatar` and `Roles` properties. If `Avatar` is null, it assigns a default URL based on the user's name or a generic "Guest" avatar. If `Roles` is null, it sets it to "User". This ensures consistent session data storage.

--- MG.Extensions/SessionExtensions.cs
+++ MG.Extensions/SessionExtensions.cs
@@ -7,6 +7,16 @@ namespace MG.Extensions.Session
     {
         public static void SetUserSessionData(this ISession session, UserSessionData userSessionData)
         {
+            if (userSessionData.Avatar == null)
+            {
+                userSessionData.Avatar = string.IsNullOrEmpty(userSessionData.Name) ? "https://ui-avatars.com/api/?name=Guest" : "https://ui-avatars.com/api/?name=" + userSessionData.Name;
+            }
+
+            if (userSessionData.Roles == null)
+            {
+                userSessionData.Roles = "User";
+            }
+
             session.SetString("UserSessionData", JsonConvert.SerializeObject(userSessionData));
         }
 
